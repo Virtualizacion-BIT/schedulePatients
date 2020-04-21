87fb060a16b2ddf983538646e7653b9953fb65a4
PaolaCuadros1 <paola.cuadroso@gmail.com>
2020-04-21 16:54:08 -0500

    Eliminado funciones modificado nombre de archivo.

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 /**
  * Creación de una constante la cual nos guardará la ruta de nuestro proyecto
  */
-include_once('funtions.php');
+include_once('functions.php');
 if ( !defined('ROOT')){
   define("ROOT", 'http://'.$_SERVER['HTTP_HOST'].getFolderProject());
 }
